175ed24f612e54deb0f5a0399bddc9f6ad73f113
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-31 00:10:39 -0300

    conectando na api currencyconverter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Conversor from './components/Conversor';
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA="USD" MoedaB="BRL"></Conversor>
+
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
 
     </div>
   );
